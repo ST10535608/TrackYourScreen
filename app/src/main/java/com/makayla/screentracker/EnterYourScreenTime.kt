6d1e26acd7e1e069a416a61807c333846b166f1c
ST10535608 <ST10535608@myemeris.edu.za>
2026-06-04 12:24:27 +0200

    Second Screen added and Intent used to move to the second screen

---
+++ app/src/main/java/com/makayla/screentracker/EnterYourScreenTime.kt
@@ -0,0 +1,20 @@
+package com.makayla.screentracker
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class EnterYourScreenTime : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_enter_your_screen_time)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
--- app/src/main/java/com/makayla/screentracker/MainActivity.kt
+++ app/src/main/java/com/makayla/screentracker/MainActivity.kt
@@ -1,6 +1,10 @@
+// Makayla Walkerley
+// ST1056306
 package com.makayla.screentracker
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +15,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Declare the two buttons we are going to use
+
+        val btnMainScreen = findViewById<Button>(R.id.btnGoMainScreen)
+        val btnExitApp = findViewById<Button>(R.id.btnExitApp)
+
+        // use setOnClickListner to move to next screen using intent
+        btnMainScreen.setOnClickListener {
+            val intent = Intent(this, EnterYourScreenTime::class.java)
+            startActivity(intent)
+        }
+
+        // close the app
+        btnExitApp.setOnClickListener {
+            finishAffinity()
         }
     }
 }
